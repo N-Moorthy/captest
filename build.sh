a13fe777486a43911e7da6938a890a1427ae25e4
My <balacruze07@outlook.com>
2024-06-22 19:33:38 +0530

    t2

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt install docker.io -y
 sudo usermod -aG docker ubuntu
-docker build -t ngimg .
-docker run --name nginx-container -d -p 3000:80  ngimg
+docker build -t myimg .
+docker run --name nginx-container -d -p 3000:80  myimg
 sudo apt install docker-compose -y
 docker-compose up -d
 docker-compose build
-docker tag ngimg sarathakashk95/prod:version-1
-docker push sarathakashk95/prod:version-1
-docker tag ngimg sarathakashk95/dev:latest
-docker push sarathakashk95/dev:latest
+docker tag myimg hanumith/prodcapstone:v1
+docker push hanumith/prodcapstone:v1
+docker tag myimg hanumith/devcapstone:v1
+docker push hanumith/devcapstone:v1
