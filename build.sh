9470c98774aa013bcb26e293e8da01156f5bece2
N Moorthy <163540678+N-Moorthy@users.noreply.github.com>
2024-06-23 23:23:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu
+
+#Docker image build step
 docker build -t myimg .
-docker run --name nginx-container -d -p 3000:80  myimg
-sudo apt install docker-compose -y
-docker-compose up -d
-docker-compose build
-docker tag myimg hanumith/prodcapstone:v1
-docker push hanumith/prodcapstone:v1
-docker tag myimg hanumith/devcapstone:v1
-docker push hanumith/devcapstone:v1
+
+#Docker compose file for container
+docker-compose up -d 
