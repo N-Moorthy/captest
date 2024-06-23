2c9c16a7cb5d4aa7e2f17c139ad8d82a8a9131b2
N Moorthy <163540678+N-Moorthy@users.noreply.github.com>
2024-06-24 02:18:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Fetching the current Git branch
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
@@ -7,14 +7,17 @@ echo "Current Git Branch: ${BRANCH_NAME}"
 # Stop and remove existing containers
 docker-compose down
 
+# Docker login
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 # Docker Prod step
-if [[ "${BRANCH_NAME}" == "Prod" ]]; then
+if [ "${BRANCH_NAME}" == "Prod" ]; then
     ./build.sh
-    docker tag capimg hanumith/prodcapstone:v1
+    docker tag myimg hanumith/prodcapstone:v1
     docker push hanumith/prodcapstone:v1
 
-elif [[ "${BRANCH_NAME}" == "Dev" ]]; then
+elif [ "${BRANCH_NAME}" == "Dev" ]; then
     ./build.sh
-    docker tag capimg hanumith/devcapstone:v1
+    docker tag myimg hanumith/devcapstone:v1
     docker push hanumith/devcapstone:v1
-
+fi
